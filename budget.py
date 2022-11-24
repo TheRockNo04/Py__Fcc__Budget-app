5459467b49013086c7bef0ed20fe1486e43dc088
TheRockNo04 <therockno04@gmail.com>
2022-11-24 16:36:13 +0530

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,7 +1,26 @@
+def create_spend_chart(categories = list()):
+    return None
+
+
+
 class Category:
-
-
-
-
-
-def create_spend_chart(categories):
+    balance = 0
+    spent = 0
+    def __init__(self, name):
+        self.name = name
+        self.ledger = list()
+    
+    def deposit(amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+    
+    def withdraw(amount, description = ""):
+        return None
+    
+    def transfer(amount, where):
+        return None
+    
+    def get_balance():
+        return None
+    
+    def check_funds():
+        return None
